cc7bc20f0d83d1cfbcc0298bb4efe5e3ecc987ae
AndrewAAG <andrewalexander.gun@gmail.com>
2026-01-06 18:16:15 +0700

    feat(talent-growth): Initialize project structure with React TS and Express

---
+++ server/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+require('dotenv').config();
+
+// Middleware
+app.use(cors());
+app.use(express.json()); // To read JSON data from request body
+
+app.get('/', (req, res) => {
+  res.send('Test Backend');
+});
+
+// Run Server
+const PORT = process.env.PORT || 5001;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
