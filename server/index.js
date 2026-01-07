0645372fc334db25ca7a1f1085a434188b180520
AndrewAAG <andrewalexander.gun@gmail.com>
2026-01-07 17:55:21 +0700

    fix(talent-growth): Allow x-auth-token header

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ app.use((req, res, next) => {
   }
 
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, x-auth-token');
   res.header('Access-Control-Allow-Credentials', 'true'); // Wajib untuk cookies/auth
 
   if (req.method === 'OPTIONS') {
